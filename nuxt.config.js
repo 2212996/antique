859e685e12ec079c1a87c111d78467f9d462ce1b
2212996 <51160826+2212996@users.noreply.github.com>
2021-04-11 22:58:47 +0900

    Add files via upload

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt_app',
+    title: 'personal gym - ANTIQUE',
     htmlAttrs: {
       lang: 'en'
     },
