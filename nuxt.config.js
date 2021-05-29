6c25c21241a50daf1680dad79683fc7b418be812
2212996 <51160826+2212996@users.noreply.github.com>
2021-05-29 23:22:46 +0900

    Add files via upload

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,23 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'personal gym - ANTIQUE',
+    title: 'パーソナルジムANTIQUE',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'ja'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'パーソナルジムANTIQUEでは，管理栄養士の資格を持つプロトレーナーがお客様一人ひとりに合わせたトレーニングとお食事内容を提案します．業界大手出身の経験を生かし，お客様の理想の体づくりをサポートいたします．' },
+      { hid: 'og:site_name', property: 'og:site_name', content: ''},
+      { hid: 'og:type', property: 'og:type', content: 'website'},
+      { hid: 'og:title', property: 'og:title', content: 'パーソナルジムANTIQUE'},
+      { hid: 'og:description', property: 'og:description', content: 'パーソナルジムANTIQUEでは，管理栄養士の資格を持つプロトレーナーがお客様一人ひとりに合わせたトレーニングとお食事内容を提案します．業界大手出身の経験を生かし，お客様の理想の体づくりをサポートいたします．'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    ],
+    description: 'パーソナルジムANTIQUEでは，管理栄養士の資格を持つプロトレーナーがお客様一人ひとりに合わせたトレーニングとお食事内容を提案します．業界大手出身の経験を生かし，お客様の理想の体づくりをサポートいたします．'
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
