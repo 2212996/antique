03778e18ae026aa48de8545cf0a9c036e6d8ad38
2212996 <51160826+2212996@users.noreply.github.com>
2021-07-04 21:12:15 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+
+jQuery(function(){
+    var windowWidth = $(window).width();
+    var headerHight = 80; 
+    jQuery('a[href^=#]').click(function() {
+    var speed = 1000;
+    var href= jQuery(this).attr("href");
+    var target = jQuery(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top-headerHight;
+    jQuery('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+   });
+});
